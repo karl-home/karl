3063fd9fcc2cbf970a0627cfb324b8f0cbd3a4ff
Gina Yuan <gina.yuan@gmail.com>
2020-08-19 16:46:52 -0700

    Import the wasmer executor

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -1,8 +1,21 @@
 // use wasmer::wasmer_runtime_core::pkg::Pkg
 use std::io::{self, Read};
 use std::net::{TcpListener, TcpStream};
+use std::path::Path;
+use wasmer::executor::{run, Run};
 
 fn handle_client(mut stream: TcpStream) -> io::Result<()> {
+    // TODO: Read the computation request from the TCP stream.
+    // TODO: Decompress the request package into a temporary directory.
+
+    // Replay the packaged computation.
+    let path = Path::new("package/");
+    let mut options = Run::new(path.to_path_buf());
+    options.replay = true;
+    run(&mut options);
+
+    // TODO: Parse the return options from the TCP stream.
+    // TODO: Serialize and return the requested results.
     Ok(())
 }
 
