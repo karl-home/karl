eb0163f932c52ebd0f2e1e7c44f153f467c5847b
Gina Yuan <gina.yuan@gmail.com>
2020-08-25 14:41:15 -0700

    Delegate requests based on type

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -5,9 +5,24 @@ use std::io::{Read, Write, BufRead};
 use std::net::{TcpListener, TcpStream};
 use std::time::Instant;
 
+use bincode;
 use wasmer::executor::{run, Run};
 use tempdir::TempDir;
 use zip;
+
+use karl::*;
+
+#[derive(Debug)]
+enum Error {
+    IoError(io::Error),
+    SerializationError(String),
+}
+
+impl From<io::Error> for Error {
+    fn from(error: io::Error) -> Self {
+        Error::IoError(error)
+    }
+}
 
 fn read_all(inner: &mut dyn Read) -> io::Result<Vec<u8>> {
     let mut buffer = Vec::new();
@@ -24,16 +39,14 @@ fn read_all(inner: &mut dyn Read) -> io::Result<Vec<u8>> {
     Ok(buffer)
 }
 
-fn handle_client(mut stream: TcpStream) -> io::Result<()> {
-    // Read the computation request from the TCP stream.
-    // WARNING: blocking
-    let now = Instant::now();
-    let buf = read_all(&mut stream)?;
-    println!("read {} bytes: {} s", buf.len(), now.elapsed().as_secs_f32());
+fn handle_ping(req: PingRequest) -> PingResult {
+    unimplemented!()
+}
 
+fn handle_compute(req: ComputeRequest) -> io::Result<ComputeResult> {
     // Decompress the request package into a temporary directory.
     let now = Instant::now();
-    let reader = io::Cursor::new(buf);
+    let reader = io::Cursor::new(req.zip);
     let mut zip = zip::ZipArchive::new(reader)?;
     let package = TempDir::new("package")?;
     for i in 0..zip.len() {
@@ -56,12 +69,34 @@ fn handle_client(mut stream: TcpStream) -> io::Result<()> {
     run(&mut options);
     println!("execution: {} s", now.elapsed().as_secs_f32());
 
-    // TODO: Parse the return options from the TCP stream.
     // TODO: Serialize and return the requested results.
+    Ok(ComputeResult::new())
+}
+
+fn handle_client(mut stream: TcpStream) -> Result<(), Error> {
+    // Read the computation request from the TCP stream.
+    let now = Instant::now();
+    let buf = read_all(&mut stream)?;
+    println!("read {} bytes: {} s", buf.len(), now.elapsed().as_secs_f32());
+
+    // Deserialize the request.
+    let req_bytes = bincode::deserialize(&buf[..])
+        .map_err(|e| Error::SerializationError(format!("{:?}", e)))?;
+
+    // Deploy the request to correct handler.
+    let res = match req_bytes {
+        KarlRequest::Ping(req) => KarlResult::Ping(handle_ping(req)),
+        KarlRequest::Compute(req) => KarlResult::Compute(handle_compute(req)?),
+    };
+
+    // Return the result to sender.
+    let res_bytes = bincode::serialize(&res)
+        .map_err(|e| Error::SerializationError(format!("{:?}", e)))?;
+    stream.write(&res_bytes[..])?;
     Ok(())
 }
 
-fn main() -> io::Result<()> {
+fn main() -> Result<(), Error> {
     let listener = TcpListener::bind("0.0.0.0:0")?;
     println!("listening on port {}", listener.local_addr()?.port());
 
