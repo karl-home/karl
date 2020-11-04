5bfbda7bcc073f787d413301a0c920b05bc3e3d5
Gina Yuan <gina.yuan@gmail.com>
2020-11-04 09:17:12 -0800

    Better dependency install scripts

--- scripts/setup_stt.sh
+++ scripts/setup_stt.sh
@@ -1,27 +1,64 @@
-STT_PATH="$HOME/.karl/local/stt"
-PYTHON=python3.6
-mkdir -p $STT_PATH
-mkdir -p $STT_PATH/bin
-echo "STT_PATH is at \"$STT_PATH\""
+#!/bin/bash
+DATA_PATH="$(pwd)/data/stt"
+if [ ! -d "$DATA_PATH" ]; then
+	echo "Data path at $DATA_PATH does not exist"
+	exit
+fi
 
-# Copy client.py
-echo "Copying client.py..."
-cp data/stt/client.py $STT_PATH/client.py
-# Copy python
-echo "Using Python installation at $(which $PYTHON)..."
-cp $(which $PYTHON) $STT_PATH/bin/python
+# Check original files exist
+check_original_file() {
+	if [ -z "$1" ]; then echo "No argument supplied"; exit; fi
+	if [ ! -f "$1" ]; then
+		echo "File does not exist at $1. Checkout the original branch?"
+		exit
+	fi
+}
+check_original_file "$DATA_PATH/client.py"
+check_original_file "$DATA_PATH/audio/2830-3980-0043.wav"
 
-# Setup python dependencies
-rm -rf $STT_PATH/lib
-virtualenv -p $PYTHON sttenv
-echo "Installing Python depenedencies..."
-source sttenv/bin/activate
-pip install deepspeech
-cp -r sttenv/lib $STT_PATH/lib
-deactivate
-rm -rf sttenv
+# Binary
+PYTHON=$(which python3.6)
+if [ -z "$PYTHON" ]; then
+ 	echo "No python3.6 installation"
+ 	exit
+else
+	mkdir -p $DATA_PATH/bin
+	echo "Using python3.6 installation at $PYTHON"
+	cp $PYTHON $DATA_PATH/bin/python
+fi
+
+# Python dependencies
+VIRTUALENV=$(which virtualenv)
+if [ -z "$VIRTUALENV" ]; then
+	echo "No virtualenv installation"
+	exit
+else
+	echo "Installing deepspeech dependency"
+	virtualenv -p $PYTHON tmp-env
+	source tmp-env/bin/activate
+	pip install deepspeech
+	rm -rf $DATA_PATH/lib
+	mv tmp-env/lib $DATA_PATH/lib
+	deactivate
+	rm -rf tmp-env
+fi
 
 # Download models
 echo "Downloading models..."
-curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $STT_PATH/models.pbmm
-curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $STT_PATH/models.scorer
+if [ ! -f $DATA_PATH/models.pbmm ]; then
+	curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $DATA_PATH/models.pbmm
+else
+	echo "$DATA_PATH/models.pbmm exists"
+fi
+if [ ! -f $DATA_PATH/models.scorer ]; then
+	curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $DATA_PATH/models.scorer
+else
+	echo "$DATA_PATH/models.scorer exists"
+fi
+
+# Copy data path to karl cache
+STT_PATH="$HOME/.karl/local/stt"
+rm -rf $STT_PATH
+cp -r $DATA_PATH $STT_PATH
+rm -rf $STT_PATH/audio
+echo "STT karl cache written to $STT_PATH"
--- scripts/setup_stt_node.sh
+++ scripts/setup_stt_node.sh
@@ -1,27 +1,80 @@
-STT_PATH="$HOME/.karl/local/stt_node"
-PYTHON=python3.6
+#!/bin/bash
+DATA_PATH="$(pwd)/data/stt_node"
+if [ ! -d "$DATA_PATH" ]; then
+	echo "Data path at $DATA_PATH does not exist"
+	exit
+fi
 
-sudo apt-get install -y npm
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-source ~/.bashrc
+# Check original files exist
+check_original_file() {
+	if [ -z "$1" ]; then echo "No argument supplied"; exit; fi
+	if [ ! -f "$1" ]; then
+		echo "File does not exist at $1. Checkout the original branch?"
+		exit
+	fi
+}
+check_original_file "$DATA_PATH/main.js"
+check_original_file "$DATA_PATH/weather.wav"
+
+# Binary
+NVM=$(which nvm)
+if [ -z "$NVM" ]; then
+	echo "No nvm installation. Trying to install."
+	if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+		sudo apt-get install -y npm
+		curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+		source ~/.bashrc
+		NVM=$(which nvm)
+		if [ -z "$NVM" ]; then
+			echo "Failed to install nvm on $OSTYPE"
+			exit
+		fi
+	elif [[ "$OSTYPE" == "darwin"* ]]; then
+		brew update && brew install nvm
+		export NVM_DIR="$HOME/.nvm"
+		[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+		[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
+	else
+		echo "Can't install nvm on $OSTYPE"
+		exit
+	fi
+fi
 nvm install 12.19.0
-rm -rf node_modules $STT_PATH/node_modules
-npm install deepspeech
+NODE=$(which node)
+[ -z "$NODE" ]
+mkdir -p $DATA_PATH/bin
+echo "Using node installation at $NODE"
+cp $NODE $DATA_PATH/bin/node
 
-mkdir -p $STT_PATH
-mkdir -p $STT_PATH/bin
-echo "STT_PATH is at \"$STT_PATH\""
-
-echo "Copying main.js..."
-cp data/stt_node/main.js $STT_PATH/main.js
-echo "Using Node (12.19.0) installation at $(which node)..."
-cp $(which node) $STT_PATH/bin/node
-
-# Setup dependencies
-mv node_modules/ $STT_PATH/node_modules/
-rm package-lock.json
+# Node dependencies
+NPM=$(which npm)
+if [ -z "$NPM" ]; then
+	echo "No npm installation"
+	exit
+else
+	echo "Installing deepspeech dependency"
+	npm install deepspeech
+	rm -rf $DATA_PATH/node_modules
+	mv node_modules $DATA_PATH/node_modules
+	rm package-lock.json
+fi
 
 # Download models
 echo "Downloading models..."
-curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $STT_PATH/models.pbmm
-curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $STT_PATH/models.scorer
+if [ ! -f $DATA_PATH/models.pbmm ]; then
+	curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.pbmm -o $DATA_PATH/models.pbmm
+else
+	echo "$DATA_PATH/models.pbmm exists"
+fi
+if [ ! -f $DATA_PATH/models.scorer ]; then
+	curl -L https://github.com/mozilla/DeepSpeech/releases/download/v0.8.1/deepspeech-0.8.1-models.scorer -o $DATA_PATH/models.scorer
+else
+	echo "$DATA_PATH/models.scorer exists"
+fi
+
+# Copy data path to karl cache
+STT_NODE_PATH="$HOME/.karl/local/stt_node"
+rm -rf $STT_NODE_PATH
+cp -r $DATA_PATH $STT_NODE_PATH
+rm -rf $STT_NODE_PATH/weather.wav
+echo "STT karl cache written to $STT_NODE_PATH"
