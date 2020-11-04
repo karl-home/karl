dd1c61c3689cd9db8c6cf1e12e8fc8d384c52d1d
Gina Yuan <gina.yuan@gmail.com>
2020-11-04 12:23:33 -0800

    Test read and write packets

--- src/common.rs
+++ src/common.rs
@@ -107,7 +107,7 @@ pub fn read_packets(
             // Split off a packet if there are enough bytes
             let nbytes = current_nbytes.unwrap();
             if current_buffer.len() >= nbytes {
-                let buffer: Vec<_> = current_buffer.drain(..).collect();
+                let buffer: Vec<_> = current_buffer.drain(..nbytes).collect();
                 result.push(buffer);
                 current_nbytes = None;
                 index += 1;
@@ -128,14 +128,12 @@ pub fn read_packets(
     }
 
     // Handle no reply, wrong number of replies
-    if result.is_empty() {
+    if result.is_empty() && current_buffer.is_empty() && current_nbytes.is_none() {
         return Err(Error::NoReply);
     }
     // Handle incorrect packet lengths
     assert!(result.len() <= npackets);
-    if result.len() == 0 {
-        Err(Error::NoReply)
-    } else if result.len() < npackets {
+    if result.len() < npackets {
         if let Some(nbytes) = current_nbytes {
             Err(Error::IncorrectPacketLength {
                 actual: current_buffer.len(),
@@ -187,4 +185,154 @@ pub fn write_packet(inner: &mut dyn Write, buffer: &Vec<u8>) -> io::Result<()> {
     inner.write_all(buffer)?;
     trace!("write success!");
     Ok(())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::fs;
+    use std::path::Path;
+
+    #[test]
+    fn write_packet_short_header() {
+        let mut buf = vec![];
+        let input = b"helloworld".to_vec();
+        match write_packet(&mut buf, &input) {
+            Ok(()) => {},
+            Err(e) => assert!(false, format!("failed to write packet: {:?}", e)),
+        }
+        assert!(HEADER_LEN > 0);
+        assert_eq!(input.len() + HEADER_LEN, buf.len(), "wrong number of bytes");
+        assert_eq!(buf[HEADER_LEN..].to_vec(), input, "input was written incorrectly");
+        let header: &[u8; HEADER_LEN] = &buf[..HEADER_LEN].try_into().unwrap();
+        let nbytes = u32::from_ne_bytes(*header);
+        assert_eq!(nbytes as usize, input.len(), "incorrect header value");
+    }
+
+    #[test]
+    fn write_packet_long_header() {
+        let mut buf = vec![];
+        let audio_file = Path::new("data/stt_node/weather.wav");
+        assert!(audio_file.exists(), "run scripts/setup_stt_node.sh");
+        let input = fs::read(audio_file).unwrap();
+        assert!(input.len() > 65535, "input length still fits in u16");
+        match write_packet(&mut buf, &input) {
+            Ok(()) => {},
+            Err(e) => assert!(false, format!("failed to write packet: {:?}", e)),
+        }
+        assert!(HEADER_LEN > 0);
+        assert_eq!(input.len() + HEADER_LEN, buf.len(), "wrong number of bytes");
+        assert_eq!(buf[HEADER_LEN..].to_vec(), input, "input was written incorrectly");
+        let header: &[u8; HEADER_LEN] = &buf[..HEADER_LEN].try_into().unwrap();
+        // if the packet length doesn't fit in u32... too bad
+        let nbytes = u32::from_ne_bytes(*header);
+        assert_eq!(nbytes as usize, input.len(), "incorrect header value");
+    }
+
+    #[test]
+    fn read_one_packet() {
+        let mut buf = vec![];
+        let mut file = fs::File::open("data/stt_node/weather.wav").unwrap();
+        let input = read_all(&mut file).unwrap();
+        assert_eq!(input.len(), 130842);
+        // assume write packet works correctly
+        write_packet(&mut buf, &input).unwrap();
+        let mut cursor = io::Cursor::new(buf);
+        let packets = match read_packets(&mut cursor, 1) {
+            Ok(packets) => packets,
+            Err(e) => {
+                assert!(false, format!("{:?}", e));
+                unreachable!();
+            },
+        };
+        assert_eq!(packets.len(), 1, "expected 1 packet");
+        assert_eq!(packets[0].len(), 130842);
+        assert_eq!(packets[0], input);
+    }
+
+    #[test]
+    fn read_two_packets() {
+        let mut buf = vec![];
+        let mut file1 = fs::File::open("data/stt_node/weather.wav").unwrap();
+        let input1 = read_all(&mut file1).unwrap();
+        assert_eq!(input1.len(), 130842);
+        let mut file2 = fs::File::open("data/stt/audio/2830-3980-0043.wav").unwrap();
+        let input2 = read_all(&mut file2).unwrap();
+        assert_eq!(input2.len(), 63244);
+        // assume write packet works correctly
+        write_packet(&mut buf, &input1).unwrap();
+        write_packet(&mut buf, &input2).unwrap();
+        assert_eq!(buf.len(), 130842 + 63244 + HEADER_LEN * 2);
+        let mut cursor = io::Cursor::new(buf);
+        let packets = match read_packets(&mut cursor, 2) {
+            Ok(packets) => packets,
+            Err(e) => {
+                assert!(false, format!("{:?}", e));
+                unreachable!();
+            },
+        };
+        assert_eq!(packets.len(), 2, "expected 2 packets");
+        assert_eq!(packets[0].len(), 130842);
+        assert_eq!(packets[0], input1);
+        assert_eq!(packets[1].len(), 63244);
+        assert_eq!(packets[1], input2);
+    }
+
+    #[test]
+    fn read_empty_buffer() {
+        let buf = vec![];
+        let mut cursor = io::Cursor::new(buf);
+        match read_packets(&mut cursor, 1) {
+            Ok(_) => assert!(false, "expected incorrect number of packets"),
+            Err(e) => match e {
+                Error::NoReply => {},
+                e => assert!(false, format!("unexpected error: {:?}", e)),
+            },
+        }
+    }
+
+    #[test]
+    fn read_too_many_packets() {
+        let mut buf = vec![];
+        let mut file = fs::File::open("data/stt/audio/2830-3980-0043.wav").unwrap();
+        let input = read_all(&mut file).unwrap();
+        assert_eq!(input.len(), 63244);
+        // assume write packet works correctly
+        write_packet(&mut buf, &input).unwrap();
+        let mut cursor = io::Cursor::new(buf);
+        match read_packets(&mut cursor, 2) {
+            Ok(_) => assert!(false, "expected incorrect number of packets"),
+            Err(e) => match e {
+                Error::IncorrectNumPackets { actual, expected } => {
+                    assert_eq!(expected, 2);
+                    assert_eq!(actual, 1);
+                },
+                e => assert!(false, format!("unexpected error: {:?}", e)),
+            },
+        }
+    }
+
+    #[test]
+    fn read_incorrect_packet_length() {
+        let mut buf = vec![];
+        let mut file = fs::File::open("data/stt_node/weather.wav").unwrap();
+        let input = read_all(&mut file).unwrap();
+        assert_eq!(input.len(), 130842);
+        write_packet(&mut buf, &input).unwrap();
+        // pretend some bytes got lost at the end
+        assert_eq!(buf.len(), 130842 + HEADER_LEN);
+        let _ = buf.split_off(130840 + HEADER_LEN);
+        assert!(buf.len() < 130842 + HEADER_LEN);
+        let mut cursor = io::Cursor::new(buf);
+        match read_packets(&mut cursor, 1) {
+            Ok(_) => assert!(false, "expected incorrect packet length"),
+            Err(e) => match e {
+                Error::IncorrectPacketLength { actual, expected } => {
+                    assert_eq!(expected, 130842);
+                    assert_eq!(actual, 130840);
+                },
+                e => assert!(false, format!("unexpected error: {:?}", e)),
+            },
+        }
+    }
 }
