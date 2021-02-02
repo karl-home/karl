bd2f608bc44feb0d8eb0cfe565a67c4b640d89b0
Gina Yuan <gina.yuan@gmail.com>
2021-02-01 16:58:20 -0800

    update detect.py from karl-cpp-sdk

--- data/person-detection/detect.py
+++ data/person-detection/detect.py
@@ -1,13 +1,14 @@
 import sys
 import time
+from datetime import datetime
 
 start = time.perf_counter()
 import torch, torchvision
 from torchvision import transforms
-from PIL import Image
+from PIL import Image, ImageDraw
 sys.stderr.write('imports\t\t%.3fs\n' % (time.perf_counter() - start))
 
-torch.hub.set_dir('torch')
+torch.hub.set_dir('storage/torch')
 model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
 model.eval()
 sys.stderr.write('init model \t%.3fs\n' % (time.perf_counter() - start))
@@ -30,7 +31,16 @@ sys.stderr.write('{}\n'.format(output['scores']))
 
 sys.stderr.write('Filtering by confidence > 0.6 and label == 1 (person)\n')
 boxes = output['boxes'].tolist()
+boxes_filtered = []
 for i in range(len(output['labels'])):
     if output['scores'][i] > 0.6 and output['labels'][i] == 1:
-        print(boxes[i])
+        boxes_filtered.append(boxes[i])
+
+sys.stderr.write('Writing output image and printing box locations\n')
+out_path = "storage/{}.jpeg".format(datetime.now()).replace(" ", "_")
+draw = ImageDraw.Draw(img)
+for box in boxes_filtered:
+    print(box)
+    draw.rectangle(box, outline="yellow", width=4)
+img.save(out_path, "JPEG")
 sys.stderr.write('log output \t%.3fs\n' % (time.perf_counter() - start))
