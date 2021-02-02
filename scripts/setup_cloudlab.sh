192e3886321716bff39b0a042eaf88a62dfeda75
Gina Yuan <gina.yuan@gmail.com>
2021-02-01 16:59:32 -0800

    update scripts

--- scripts/setup_cloudlab.sh
+++ scripts/setup_cloudlab.sh
@@ -27,6 +27,19 @@ build() {
 	cargo b --release --bin host
 }
 
+copy_to_storage() {
+	export LOCAL="$HOME/.karl/storage"
+	mkdir -p $LOCAL/wyzecam $LOCAL/almond
+
+	cp -r data/person-detection/* $LOCAL/wyzecam
+	rm -r $LOCAL/wyzecam/PennFudanPed
+	rm $LOCAL/wyzecam/setup.sh
+
+	cp -r data/stt_node/* $LOCAL/almond
+	rm $LOCAL/almond/weather.wav
+	rm $LOCAL/almond/setup.sh
+}
+
 install_deps
 init_submodules
 build
@@ -35,19 +48,15 @@ source setup.sh
 cd ../stt_node
 source setup.sh
 cd ../..
+copy_to_storage
 
+build_cpp_sdk() {
+	cd karl-cpp-sdk
+	./generate.sh
+	./linux.sh
+	$(./register-linux data/index.hbs node0 59582 2>/dev/null)
+	./person_detection-linux data/detect.py data/img.tmp node0 59582
+}
 
-# copy data to karl cache
-export LOCAL="$HOME/.karl/local/"
-mkdir -p $LOCAL
-
-cp -r data/person-detection $LOCAL
-rm -r $LOCAL/person-detection/PennFudanPed
-rm $LOCAL/person-detection/setup.sh
-
-cp -r data/stt_node $LOCAL
-rm $LOCAL/stt_node/weather.wav
-rm $LOCAL/stt_node/setup.sh
-
-RUST_LOG=hyper::server=info,debug ./target/debug/controller --karl-path /users/ygina/.karl_controller
+RUST_LOG=hyper::server=info,debug ./target/debug/controller --karl-path /users/ygina/.karl_controller --autoconfirm
 sudo RUST_LOG=debug ./target/release/host -b binary --karl-path /users/ygina/.karl
