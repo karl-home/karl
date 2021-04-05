5a625132702de6ae17cf6735185c05e7fe3138a5
Gina Yuan <gina.yuan@gmail.com>
2021-04-05 14:17:53 -0700

    Delete distributed CloudLab scripts

--- scripts/kill.sh
+++
@@ -1,7 +0,0 @@
-cat $HOME/karl/hosts.txt | while read machine
-do
-    echo "killing karl services on machine $USER@${machine}"
-    ssh $USER@${machine} "ps -ef | grep 'karl/target' | grep -v grep | awk '{print \$2}'" &
-    ssh $USER@${machine} \
-        "ps -ef | grep 'karl/target' | grep -v grep | awk '{print \$2}' | xargs -r kill -9" &
-done
