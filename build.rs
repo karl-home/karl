9f93d6e447cebe9607027a03d8d7b382ea34847e
Gina Yuan <gina.yuan@gmail.com>
2021-04-05 14:17:04 -0700

    Build Rust protobuf directly

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ extern crate protoc_rust;
 
 fn main() {
     protoc_rust::Codegen::new()
-        .out_dir("protos/rust")
+        .out_dir("src/protos")
         .inputs(&["protos/request.proto"])
         .include("protos")
         .run()
