eb9eaea32c26919c7fb194a140de363d012c9bd9
Gina Yuan <gina.yuan@gmail.com>
2021-04-05 17:30:54 -0700

    Consolidate scripts
    Separate cloudlab script into install_dependencies.sh and
    install_experiment_data.sh. Second script also inits dependencies
    for the hello world example.

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+install_deps() {
+	# Rust
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	# Select 1) Proceed with installation
+	source $HOME/.cargo/env
+	rustup toolchain install nightly
+	rustup default nightly
+
+	# Other deps
+	sudo apt update
+	sudo apt install -y protobuf-compiler python3-pip virtualenv npm
+
+	# nvm
+	curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | sh
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+}
+
+init_submodules() {
+	git submodule init
+	git submodule update
+}
+
+build_karl() {
+	cargo b --bin controller
+	cargo b --release --bin host
+}
+
+build_cpp_sdk() {
+	cd karl-cpp-sdk
+	./generate.sh
+	cd ..
+}
+
+build_node_sdk() {
+	cd karl-node-example
+	source init.sh
+	cd ..
+}
+
+install_deps
+init_submodules
+build_karl
+build_cpp_sdk
+build_node_sdk
