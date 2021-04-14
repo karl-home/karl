2bd808397526ab62462c23ca2e450f12b4d9d698
Gina Yuan <gina.yuan@gmail.com>
2021-04-13 17:10:19 -0700

    host handling of requests to read/write files and access network

--- src/host.rs
+++ src/host.rs
@@ -1,17 +1,18 @@
 use std::fs;
-use std::collections::HashMap;
+use std::collections::{HashSet, HashMap};
 use std::path::{Path, PathBuf};
 use std::time::{Duration, Instant};
 use std::sync::{Arc, Mutex};
 
 use tokio;
+use reqwest::{self, Method, header::HeaderName};
 use flate2::read::GzDecoder;
 use tar::Archive;
 
 use tonic::{Request, Response, Status, Code};
+use crate::protos::karl_controller_client::KarlControllerClient;
 use crate::protos::*;
 use crate::common::*;
-use crate::hook::FileACL;
 
 /// Frequency at which the host must send messages to the controller, in seconds.
 pub const HEARTBEAT_INTERVAL: u64 = 10;
@@ -19,9 +20,52 @@ pub const HEARTBEAT_INTERVAL: u64 = 10;
 /// Permissions of an active process
 struct ProcessPerms {
     /// Network permissions
-    network_perms: Vec<String>,
-    /// File permissions
-    file_perms: Vec<FileACL>,
+    network_perms: HashSet<String>,
+    /// File read permissions
+    read_perms: HashSet<PathBuf>,
+    /// File write permissions
+    write_perms: HashSet<PathBuf>,
+}
+
+impl ProcessPerms {
+    pub fn new(req: &ComputeRequest) -> Self {
+        let network_perms: HashSet<_> = req.network_perm.clone().into_iter().collect();
+        let read_perms: HashSet<_> = req.file_perm.iter()
+            .filter(|acl| acl.read)
+            .map(|acl| Path::new(&acl.path).to_path_buf())
+            .collect();
+        let write_perms: HashSet<_> = req.file_perm.iter()
+            .filter(|acl| acl.write)
+            .map(|acl| Path::new(&acl.path).to_path_buf())
+            .collect();
+        Self { network_perms, read_perms, write_perms }
+    }
+
+    pub fn can_access_domain(&self, domain: &str) -> bool {
+        self.network_perms.contains(domain)
+    }
+
+    pub fn can_read_file(&self, path: &Path) -> bool {
+        let mut next_path = Some(path);
+        while let Some(path) = next_path {
+            if self.read_perms.contains(path) {
+                return true;
+            }
+            next_path = path.parent();
+        }
+        false
+    }
+
+    pub fn can_write_file(&self, path: &Path) -> bool {
+        let mut next_path = Some(path);
+        while let Some(path) = next_path {
+            if self.write_perms.contains(path) {
+                return true;
+            }
+            next_path = path.parent();
+        }
+        false
+    }
 }
 
 pub struct Host {
@@ -47,10 +91,7 @@ impl karl_host_server::KarlHost for Host {
     ) -> Result<Response<NotifyStart>, Status> {
         let req = req.into_inner();
         let process_token = Token::gen();
-        let perms = ProcessPerms {
-            network_perms: req.network_perm.clone(),
-            file_perms: req.file_perm.iter().map(|acl| FileACL::from(acl)).collect(),
-        };
+        let perms = ProcessPerms::new(&req);
 
         // Mark an active process
         let mut process_tokens = self.process_tokens.lock().unwrap();
@@ -67,7 +108,6 @@ impl karl_host_server::KarlHost for Host {
             let process_tokens = self.process_tokens.clone();
             let host_token = self.host_token.clone().ok_or(
                 Status::new(Code::Unavailable, "host is not registered with controller"))?;
-            let id = self.id;
             let process_token = process_token.clone();
             tokio::spawn(async move {
                 Host::handle_compute(
@@ -89,25 +129,143 @@ impl karl_host_server::KarlHost for Host {
     async fn network(
         &self, req: Request<NetworkAccess>,
     ) -> Result<Response<NetworkAccessResult>, Status> {
-        unimplemented!()
+        // Validate the process is valid and has permissions to access the network.
+        // No serializability guarantees from other requests from the same process.
+        let mut req = req.into_inner();
+        if let Some(perms) = self.process_tokens.lock().unwrap().get(&req.process_token) {
+            if !perms.can_access_domain(&req.domain) {
+                return Err(Status::new(Code::Unauthenticated, "invalid ACL"));
+            }
+        } else {
+            return Err(Status::new(Code::Unauthenticated, "invalid process token"));
+        }
+
+        // Build the network request
+        let method = match req.method.as_str() {
+            "GET" => Method::GET,
+            "POST" => Method::POST,
+            "PUT" => Method::PUT,
+            "DELETE" => Method::DELETE,
+            "HEAD" => Method::HEAD,
+            "OPTIONS" => Method::OPTIONS,
+            "CONNECT" => Method::CONNECT,
+            "PATCH" => Method::PATCH,
+            "TRACE" => Method::TRACE,
+            _ => { return Err(Status::new(Code::InvalidArgument, "invalid http method")) },
+        };
+        let mut builder = reqwest::Client::new()
+            .request(method, req.domain.clone())
+            .body(req.body.clone());
+        for header in &req.headers {
+            let key = HeaderName::from_bytes(&header.key[..])
+                .map_err(|e| {
+                    error!("{}", e);
+                    Status::new(Code::InvalidArgument, "invalid header name")
+                })?;
+            builder = builder.header(key, &header.value[..]);
+        }
+
+        // Make the actual network access
+        let handle = tokio::spawn(async move {
+            let res = builder.send().await.map_err(|e| {
+                error!("{}", e);
+                Status::new(Code::Aborted, "http request failed")
+            })?;
+            let status_code = res.status().as_u16() as u32;
+            let headers = res
+                .headers()
+                .iter()
+                .map(|(key, value)| KeyValuePair {
+                    key: key.as_str().as_bytes().to_vec(),
+                    value: value.as_bytes().to_vec(),
+                })
+                .collect::<Vec<_>>();
+            let data = res.bytes().await.map_err(|e| {
+                error!("{}", e);
+                Status::new(Code::Unavailable, "error streaming response bytes")
+            })?;
+            Ok(Response::new(NetworkAccessResult {
+                status_code,
+                headers,
+                data: data.to_vec(),
+            }))
+        });
+
+        // Forward the network access to the controller.
+        req.host_token = self.host_token.clone().ok_or(
+            Status::new(Code::Unavailable, "host is not registered with controller"))?;
+        KarlControllerClient::connect(self.controller.clone()).await
+            .map_err(|e| Status::new(Code::Internal, format!("{:?}", e)))?
+            .forward_network(Request::new(req)).await?;
+
+        // Return the result of the HTTP request.
+        handle.await.map_err(|e| Status::new(Code::Internal, format!("{}", e)))?
     }
 
     async fn get(
         &self, req: Request<GetData>,
     ) -> Result<Response<GetDataResult>, Status> {
-        unimplemented!()
+        // Validate the process is valid and has permissions to read the file.
+        // No serializability guarantees from other requests from the same process.
+        let mut req = req.into_inner();
+        if let Some(perms) = self.process_tokens.lock().unwrap().get(&req.process_token) {
+            if !perms.can_read_file(Path::new(&req.path)) {
+                return Err(Status::new(Code::Unauthenticated, "invalid ACL"));
+            }
+        } else {
+            return Err(Status::new(Code::Unauthenticated, "invalid process token"));
+        }
+
+        // Forward the file access to the controller and return the result
+        req.host_token = self.host_token.clone().ok_or(
+            Status::new(Code::Unavailable, "host is not registered with controller"))?;
+        KarlControllerClient::connect(self.controller.clone()).await
+            .map_err(|e| Status::new(Code::Internal, format!("{:?}", e)))?
+            .forward_get(Request::new(req)).await
     }
 
     async fn put(
         &self, req: Request<PutData>,
     ) -> Result<Response<()>, Status> {
-        unimplemented!()
+        // Validate the process is valid and has permissions to write the file.
+        // No serializability guarantees from other requests from the same process.
+        let mut req = req.into_inner();
+        if let Some(perms) = self.process_tokens.lock().unwrap().get(&req.process_token) {
+            if !perms.can_write_file(Path::new(&req.path)) {
+                return Err(Status::new(Code::Unauthenticated, "invalid ACL"));
+            }
+        } else {
+            return Err(Status::new(Code::Unauthenticated, "invalid process token"));
+        }
+
+        // Forward the file access to the controller and return the result
+        req.host_token = self.host_token.clone().ok_or(
+            Status::new(Code::Unavailable, "host is not registered with controller"))?;
+        KarlControllerClient::connect(self.controller.clone()).await
+            .map_err(|e| Status::new(Code::Internal, format!("{:?}", e)))?
+            .forward_put(Request::new(req)).await
     }
 
     async fn delete(
         &self, req: Request<DeleteData>,
     ) -> Result<Response<()>, Status> {
-        unimplemented!()
+        // Validate the process is valid and has permissions to write the file.
+        // No serializability guarantees from other requests from the same process.
+        let mut req = req.into_inner();
+        if let Some(perms) = self.process_tokens.lock().unwrap().get(&req.process_token) {
+            if !perms.can_write_file(Path::new(&req.path)) {
+                return Err(Status::new(Code::Unauthenticated, "invalid ACL"));
+            }
+        } else {
+            return Err(Status::new(Code::Unauthenticated, "invalid process token"));
+        }
+
+        // Forward the file access to the controller and return the result
+        req.host_token = self.host_token.clone().ok_or(
+            Status::new(Code::Unavailable, "host is not registered with controller"))?;
+        KarlControllerClient::connect(self.controller.clone()).await
+            .map_err(|e| Status::new(Code::Internal, format!("{:?}", e)))?
+            .forward_delete(Request::new(req)).await
     }
 
     async fn state(
