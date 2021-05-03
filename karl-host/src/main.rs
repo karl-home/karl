db58dfb4906cba513ddd51c4700b9f14a5fff0a8
Gina Yuan <gina.yuan@gmail.com>
2021-05-03 15:21:07 -0700

    remove more stuff

--- karl-host/src/main.rs
+++ karl-host/src/main.rs
@@ -1,6 +1,23 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(custom_inner_attributes)]
+
+#[macro_use]
+extern crate log;
+
+pub mod net;
+pub mod common;
 mod runtime;
 mod cache;
 mod perms;
+use common::*;
+use cache::PathManager;
+use perms::ProcessPerms;
+
+pub mod protos {
+	tonic::include_proto!("request");
+}
+use protos::karl_host_server::KarlHostServer;
+use protos::*;
 
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
@@ -9,12 +26,9 @@ use std::sync::{Arc, Mutex};
 
 use tokio;
 use reqwest::{self, Method, header::HeaderName};
-
 use tonic::{Request, Response, Status, Code};
-use crate::protos::*;
-use crate::common::*;
-use perms::ProcessPerms;
-use cache::PathManager;
+use tonic::transport::Server;
+use clap::{Arg, App};
 
 /// Frequency at which the host must send messages to the controller, in seconds.
 pub const HEARTBEAT_INTERVAL: u64 = 10;
@@ -310,4 +324,54 @@ impl Host {
         );
         Ok(())
     }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    env_logger::builder().init();
+    let matches = App::new("Karl Host")
+        .arg(Arg::with_name("karl-path")
+            .help("Absolute path to the base Karl directory.")
+            .long("karl-path")
+            .takes_value(true)
+            .default_value("/home/gina/.karl"))
+        .arg(Arg::with_name("port")
+            .help("Port. Defaults to a random open port.")
+            .short("p")
+            .long("port")
+            .takes_value(true)
+            .default_value("59583"))
+        .arg(Arg::with_name("password")
+            .help("Controller password to register host.")
+            .long("password")
+            .takes_value(true)
+            .default_value("password"))
+        .arg(Arg::with_name("controller-ip")
+            .help("IP address of the controller")
+            .long("controller-ip")
+            .takes_value(true)
+            .default_value("127.0.0.1"))
+        .arg(Arg::with_name("controller-port")
+            .help("Port of the controller")
+            .long("controller-port")
+            .takes_value(true)
+            .default_value("59582"))
+        .get_matches();
+
+    let karl_path = Path::new(matches.value_of("karl-path").unwrap()).to_path_buf();
+    let port: u16 = matches.value_of("port").unwrap().parse().unwrap();
+    let controller = format!(
+        "http://{}:{}",
+        matches.value_of("controller-ip").unwrap(),
+        matches.value_of("controller-port").unwrap(),
+    );
+    let password = matches.value_of("password").unwrap();
+    let mut host = Host::new(karl_path, &controller);
+    host.start(port, password).await.unwrap();
+    Server::builder()
+        .add_service(KarlHostServer::new(host))
+        .serve(format!("0.0.0.0:{}", port).parse()?)
+        .await
+        .unwrap();
+    Ok(())
 }
