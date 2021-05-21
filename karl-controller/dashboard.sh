4103b734ea8865d490a51c98e63576d457e1e767
Gina Yuan <gina.yuan@gmail.com>
2021-05-21 13:46:02 -0700

    RUST_BACKTRACE=1 for controller and host errors

--- karl-controller/dashboard.sh
+++ karl-controller/dashboard.sh
@@ -1,1 +1,1 @@
-RUST_LOG=info,karl=debug ./target/release/karl-controller --dashboard --pubsub-enabled 1 --caching-enabled 1
+RUST_BACKTRACE=1 RUST_LOG=info,karl=debug ./target/release/karl-controller --dashboard --pubsub-enabled 1 --caching-enabled 1 --autoconfirm
--- karl-host/start.sh
+++ karl-host/start.sh
@@ -12,7 +12,7 @@ elif [ $1 -eq "2" ]; then
 elif [ $1 -eq "3" ]; then
 	sudo RUST_LOG=h2=info,hyper=info,tower=info,debug ./target/release/karl-host --port 59583 --pubsub 1 --cold-cache 1 --warm-cache 0
 elif [ $1 -eq "4" ]; then
-	sudo RUST_LOG=h2=info,hyper=info,tower=info,debug ./target/release/karl-host --port 59583 --pubsub 1 --cold-cache 1 --warm-cache 1
+	sudo RUST_BACKTRACE=1 RUST_LOG=h2=info,hyper=info,tower=info,debug ./target/release/karl-host --port 59583 --pubsub 1 --cold-cache 1 --warm-cache 1
 else
 	echo "1=none,2=pubsub,3=cold,4=warm"
 fi
