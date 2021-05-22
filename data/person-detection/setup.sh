2685a98457e2636b8c088cd0da5b17c6e9ae5fce
Gina Yuan <gina.yuan@gmail.com>
2021-05-22 11:41:48 -0700

    update python module setup scripts

--- data/person-detection/setup.sh
+++ data/person-detection/setup.sh
@@ -1,8 +1,7 @@
 # Setup Python virtual environment
 virtualenv -p python3 env
 source env/bin/activate
-pip install grpcio grpcio-tools
-pip install torch torchvision
+pip install -r requirements.txt
 
 # Generate the gRPC wrappers
 python -m grpc_tools.protoc -I../../protos \
@@ -20,3 +19,14 @@ mv maskrcnn_resnet50_fpn_coco-bf2d0c1e.pth torch/checkpoints/
 
 # Deactivate
 deactivate
+
+run_firejail() {
+    firejail --private=. \
+    --netfilter=/etc/firejail/karl.net \
+    --env=GLOBAL_MODULE_ID=person_detection \
+    --env=MODULE_ID=person_detection-2917637381 \
+    --env=TRIGGERED_TAG=image \
+    --env=PROCESS_TOKEN=7mb7zUL4s9Gs2F2WTe6v0qrFRemXuOgy \
+    --env=TRIGGERED_TIMESTAMP=2021-04-26T14:49:04.499119935-07:00 \
+    ./env/bin/python detect.py
+}
