c9b753be03544764063d8e2870cfdd5dfefe0039
Gina Yuan <gina.yuan@gmail.com>
2021-05-25 16:20:05 -0700

    cleanup old handlebars code

--- karl-controller/src/old_dashboard/mod.rs
+++ karl-controller/src/old_dashboard/mod.rs
@@ -58,70 +58,8 @@ fn index(
     }
 }
 
-// TODO: authenticate this request. Only the homeowner can call this.
-#[post("/confirm/host/<service_name>")]
-fn confirm_host(
-    host_header: HostHeader,
-    base_domain: State<String>,
-    service_name: String,
-    hosts: State<Arc<Mutex<HostScheduler>>>,
-) {
-    if to_client_id(&host_header, base_domain.to_string()).is_some() {
-        // Confirm must be to the base domain only.
-        return;
-    }
-    hosts.lock().unwrap().confirm_host(&service_name);
-}
-
-// TODO: authenticate this request. Only the homeowner can call this.
-/// User confirms the client with this name, indicating it is trusted.
-///
-/// The endpoint additionally writes the client to `<KARL_PATH>/clients.txt`
-/// so the client will automatically re-register if the controller restarts.
-#[post("/confirm/client/<client_id>")]
-fn confirm_client(
-    host_header: HostHeader,
-    karl_path: State<PathBuf>,
-    base_domain: State<String>,
-    client_id: String,
-    sensors: State<Arc<Mutex<HashMap<SensorToken, Client>>>>,
-) {
-    if to_client_id(&host_header, base_domain.to_string()).is_some() {
-        // Confirm must be to the base domain only.
-        return;
-    }
-    let mut sensors = sensors.lock().unwrap();
-    for (token, client) in sensors.iter_mut() {
-        if client.id != client_id {
-            continue;
-        }
-        if client.confirmed {
-            warn!("attempted to confirm already confirmed client: {:?}", client_id);
-            return;
-        } else {
-            // Create the client file if it does not already exist.
-            let path = karl_path.join("clients.txt");
-            let mut file = fs::OpenOptions::new()
-                .append(true)
-                .create(true)
-                .open(&path)
-                .expect(&format!("unable to open sensors file: {:?}", &path));
-            let res = writeln!(file, "{}:{}={}", client.id, client.addr, token);
-            if let Err(e) = res {
-                error!("error serializing client {:?}: {:?}", client, e);
-            }
-            info!("confirmed client {:?}", client_id);
-            client.confirmed = true;
-            return;
-        }
-    }
-    warn!("attempted to confirm nonexistent client: {:?}", client_id);
-}
-
 pub fn start(
     karl_path: PathBuf,
-    hosts: Arc<Mutex<HostScheduler>>,
-    sensors: Arc<Mutex<HashMap<SensorToken, Client>>>,
 ) {
     let base_domain = "karl.zapto.org".to_string();
     let sessions = Arc::new(Mutex::new(SessionState::new()));
@@ -129,10 +67,8 @@ pub fn start(
         rocket::ignite()
         .manage(karl_path)
         .manage(base_domain)
-        .manage(hosts)
-        .manage(sensors)
         .manage(sessions)
-        .mount("/", routes![index, confirm_host, confirm_client])
+        .mount("/", routes![index])
         .mount("/", routes![client::storage, client::proxy_get])
         .attach(Template::custom(|engines| {
             engines.handlebars.register_helper("request", Box::new(request_helper));
